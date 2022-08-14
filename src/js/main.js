c9827fbe983f6ec37a4f77fd3b5b1d78c77d60d9
Joanna <jlapa.transport@gmail.com>
2022-08-15 01:28:45 +0200

    'email validation functionality finished'

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,46 @@ const primaryNavigation = document.querySelector('.primary-nav')
 const headerContainer = document.querySelector('.header__container')
 const openingIcon = document.querySelector('.nav__mobile-icon--open')
 const closingIcon = document.querySelector('.nav__mobile-icon--close')
+const footerInput = document.querySelector('.footer__input')
+const msgText = document.querySelector('.msgtext')
+const inputErrorStyles = document.querySelector('.footer__input--error')
+const submitButton = document.querySelector('.button--small')
 
 const switchMobileNavigation = () => {
-    headerContainer.hasAttribute('data-visible')
-    ? toggleButton.setAttribute('aria-expanded', 'false')
-    : toggleButton.setAttribute('aria-expanded', 'true')
-headerContainer.toggleAttribute('data-overlay')
-primaryNavigation.toggleAttribute('data-visible')
-openingIcon.toggleAttribute('data-visible')
-closingIcon.toggleAttribute('data-visible')
+	headerContainer.hasAttribute('data-visible')
+		? toggleButton.setAttribute('aria-expanded', 'false')
+		: toggleButton.setAttribute('aria-expanded', 'true')
+	headerContainer.toggleAttribute('data-overlay')
+	primaryNavigation.toggleAttribute('data-visible')
+	openingIcon.toggleAttribute('data-visible')
+	closingIcon.toggleAttribute('data-visible')
 }
 
+const makeIfCorrect = () => {
+	footerInput.classList.remove('footer__input--error')
+	msgText.textContent = 'Your email was added successfully!'
+	msgText.setAttribute('data-visible', 'true')
+	footerInput.setAttribute('aria-invalid', 'false')
+	footerInput.value = ''
+}
+
+const makeIfNotCorrect = () => {
+	msgText.textContent = 'Please insert a valid e-mail'
+	msgText.setAttribute('data-visible', 'true')
+	footerInput.classList.add('footer__input--error')
+	footerInput.setAttribute('aria-invalid', 'true')
+	footerInput.value = ''
+}
+
+const checkIfCorrect = input => {
+	const re =
+		/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+	re.test(input.value) ? makeIfCorrect() : makeIfNotCorrect()
+}
+
+submitButton.addEventListener('click', e => {
+	e.preventDefault()
+	checkIfCorrect(footerInput)
+})
 toggleButton.addEventListener('click', switchMobileNavigation)
