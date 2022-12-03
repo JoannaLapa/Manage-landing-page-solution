726bf3d81c9aead7c2b9e6a2631c49cea99d0170
Joanna <jlapa.transport@gmail.com>
2022-12-03 17:06:39 +0100

    code formatter

--- src/js/main.js
+++ src/js/main.js
@@ -10,67 +10,66 @@
 	const submitButton = document.querySelector('.button--small');
 	const toggleButton = document.querySelector('.nav__mobile-toggle-button');
 
+	/////MOBILE NAVIGATION//////////////////////////////////
+	const switchMobileNavigation = () => {
+		primaryNavigation.hasAttribute('data-visible')
+			? toggleButton.setAttribute('aria-expanded', 'false')
+			: toggleButton.setAttribute('aria-expanded', 'true');
+		headerContainer.toggleAttribute('data-overlay');
+		primaryNavigation.toggleAttribute('data-visible');
+		openingIcon.toggleAttribute('data-visible');
+		closingIcon.toggleAttribute('data-visible');
+		document.querySelector('body').classList.toggle('block-scroll');
+	};
 
-/////MOBILE NAVIGATION//////////////////////////////////
-const switchMobileNavigation = () => {
-	primaryNavigation.hasAttribute('data-visible')
-		? toggleButton.setAttribute('aria-expanded', 'false')
-		: toggleButton.setAttribute('aria-expanded', 'true');
-	headerContainer.toggleAttribute('data-overlay');
-	primaryNavigation.toggleAttribute('data-visible');
-	openingIcon.toggleAttribute('data-visible');
-	closingIcon.toggleAttribute('data-visible');
-	document.querySelector('body').classList.toggle('block-scroll');
-};
-
-////////////////////////////////////////////
-// SLIDER
-const slider = new A11YSlider(document.querySelector('.slider'), {
-	autoplaySpeed: 2500,
-	adaptiveHeight: true,
-	centerMode: true,
-	dots: true,
-	arrows: false,
-	autoplay: true,
-	responsive: {
-		768: {
-			dots: false,
-			slidesToShow: 2,
+	////////////////////////////////////////////
+	// SLIDER
+	const slider = new A11YSlider(document.querySelector('.slider'), {
+		autoplaySpeed: 2500,
+		adaptiveHeight: true,
+		centerMode: true,
+		dots: true,
+		arrows: false,
+		autoplay: true,
+		responsive: {
+			768: {
+				dots: false,
+				slidesToShow: 2,
+			},
+			1200: {
+				slidesToShow: null,
+			},
 		},
-		1200: {
-			slidesToShow: null,
-		},
-	},
-});
+	});
 
-// /////////////////////////////////////////
-// EMAIL VALIDATION (CTA SECTION)
-const makeIfCorrect = () => {
-	footerInput.classList.remove('footer__input--error');
-	msgText.textContent = 'Your email was added successfully!';
-	footerInput.setAttribute('aria-invalid', 'false');
-	footerInput.value = '';
-};
+	// /////////////////////////////////////////
+	// EMAIL VALIDATION (CTA SECTION)
+	const makeIfCorrect = () => {
+		footerInput.classList.remove('footer__input--error');
+		msgText.textContent = 'Your email was added successfully!';
+		footerInput.setAttribute('aria-invalid', 'false');
+		footerInput.value = '';
+	};
 
-const makeIfNotCorrect = () => {
-	msgText.textContent = 'Please insert a valid e-mail';
-	footerInput.classList.add('footer__input--error');
-	footerInput.setAttribute('aria-invalid', 'true');
-	footerInput.value = '';
-};
+	const makeIfNotCorrect = () => {
+		msgText.textContent = 'Please insert a valid e-mail';
+		footerInput.classList.add('footer__input--error');
+		footerInput.setAttribute('aria-invalid', 'true');
+		footerInput.value = '';
+	};
 
-const checkIfCorrect = input => {
-	const re =
-		/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+	const checkIfCorrect = input => {
+		const re =
+			/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-	re.test(input.value) ? makeIfCorrect() : makeIfNotCorrect();
-};
+		re.test(input.value) ? makeIfCorrect() : makeIfNotCorrect();
+	};
 
-////ADDEVENTLISTENERS
-submitButton.addEventListener('click', e => {
-	e.preventDefault();
-	checkIfCorrect(footerInput);
-});
+	////ADDEVENTLISTENERS
+	submitButton.addEventListener('click', e => {
+		e.preventDefault();
+		checkIfCorrect(footerInput);
+	});
 
-toggleButton.addEventListener('click', switchMobileNavigation);
+	toggleButton.addEventListener('click', switchMobileNavigation);
 })();
